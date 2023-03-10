2daec8f9d0fd49c2378a1098bada9debc9ae056b
Eduardo Antonio Pérez <eduardoperez@gmail.com>
2023-03-10 05:06:58 -0600

    Commit Proyecto

---
+++ Facturacion/Vista/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if (UsuarioTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese un usuario");
+                UsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(ContraseñaTextBox.Text))
+            {
+                errorProvider1.SetError(ContraseñaTextBox, "Ingrese una contraseña");
+                ContraseñaTextBox.Clear();
+                ContraseñaTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+            //Validar en la Base de Datos
+
+
+            //Mostramos el Menú
+
+            Menu menuFormulario = new Menu();
+            Hide();
+            menuFormulario.Show();
+        }
+
+        private void MostrarContraseñaButton_Click(object sender, EventArgs e)
+        {
+            if (ContraseñaTextBox.PasswordChar == '*')
+            {
+                ContraseñaTextBox.PasswordChar = '\0';
+            }
+            else
+            {
+                ContraseñaTextBox.PasswordChar = '*';
+            }
+        }
+    }
+}
---
+++ Facturacion/Vista/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    internal static class Program
+    {
+
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTI4MzQyM0AzMjMwMmUzNDJlMzBQUHR2OGVaUEhlN1NKNlp3ZS8zMytmaS9oNW1EcTk5K09iblVuaUxHM0ljPQ==;Mgo+DSMBaFt/QHRqVVhjVFpFdEBBXHxAd1p/VWJYdVt5flBPcDwsT3RfQF5jSn5bd0BjW35Yd3FXQQ==;MTI4MzQyNUAzMjMwMmUzNDJlMzBtVXYxdEV2TWVTSGxCRGpvL3NidE5kS3VyNWZVS3F6QW11R2dJZjREYm9JPQ==");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
