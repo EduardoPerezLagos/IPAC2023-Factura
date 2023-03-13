c3bd15c28fbb7e97a5c992d73ed13f67c68da12b
Eduardo Antonio Pérez <eduardoperez@gmail.com>
2023-03-12 23:38:56 -0600

    Commit Proyecto Factura

--- Facturacion/Vista/ClientesForm.Designer.cs
+++ Facturacion/Vista/ClientesForm.Designer.cs
@@ -29,11 +29,275 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ClientesDataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.IdentidadTextBox = new System.Windows.Forms.TextBox();
+            this.TelefonoTextBox = new System.Windows.Forms.TextBox();
+            this.CorreoTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DireccionTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.EstaActivoCheckBox = new System.Windows.Forms.CheckBox();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.ModificarButton = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.FechaNacimientoDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClientesDataGridView
+            // 
+            this.ClientesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientesDataGridView.Location = new System.Drawing.Point(-4, 279);
+            this.ClientesDataGridView.Name = "ClientesDataGridView";
+            this.ClientesDataGridView.Size = new System.Drawing.Size(808, 173);
+            this.ClientesDataGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Identidad:";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Enabled = false;
+            this.NombreTextBox.Location = new System.Drawing.Point(118, 62);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(289, 20);
+            this.NombreTextBox.TabIndex = 3;
+            // 
+            // IdentidadTextBox
+            // 
+            this.IdentidadTextBox.Enabled = false;
+            this.IdentidadTextBox.Location = new System.Drawing.Point(118, 27);
+            this.IdentidadTextBox.Name = "IdentidadTextBox";
+            this.IdentidadTextBox.Size = new System.Drawing.Size(289, 20);
+            this.IdentidadTextBox.TabIndex = 4;
+            // 
+            // TelefonoTextBox
+            // 
+            this.TelefonoTextBox.Enabled = false;
+            this.TelefonoTextBox.Location = new System.Drawing.Point(118, 97);
+            this.TelefonoTextBox.Name = "TelefonoTextBox";
+            this.TelefonoTextBox.Size = new System.Drawing.Size(289, 20);
+            this.TelefonoTextBox.TabIndex = 5;
+            // 
+            // CorreoTextBox
+            // 
+            this.CorreoTextBox.Enabled = false;
+            this.CorreoTextBox.Location = new System.Drawing.Point(118, 133);
+            this.CorreoTextBox.Name = "CorreoTextBox";
+            this.CorreoTextBox.Size = new System.Drawing.Size(289, 20);
+            this.CorreoTextBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Telefono:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Correo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Dirección:";
+            // 
+            // DireccionTextBox
+            // 
+            this.DireccionTextBox.Enabled = false;
+            this.DireccionTextBox.Location = new System.Drawing.Point(118, 169);
+            this.DireccionTextBox.Name = "DireccionTextBox";
+            this.DireccionTextBox.Size = new System.Drawing.Size(289, 20);
+            this.DireccionTextBox.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Esta Activo";
+            // 
+            // EstaActivoCheckBox
+            // 
+            this.EstaActivoCheckBox.AutoSize = true;
+            this.EstaActivoCheckBox.Enabled = false;
+            this.EstaActivoCheckBox.Location = new System.Drawing.Point(118, 207);
+            this.EstaActivoCheckBox.Name = "EstaActivoCheckBox";
+            this.EstaActivoCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.EstaActivoCheckBox.TabIndex = 13;
+            this.EstaActivoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.Enabled = false;
+            this.CancelarButton.Location = new System.Drawing.Point(601, 246);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(75, 27);
+            this.CancelarButton.TabIndex = 23;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Enabled = false;
+            this.EliminarButton.Location = new System.Drawing.Point(520, 246);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 27);
+            this.EliminarButton.TabIndex = 22;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            this.EliminarButton.Click += new System.EventHandler(this.EliminarButton_Click);
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Enabled = false;
+            this.GuardarButton.Location = new System.Drawing.Point(438, 246);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(75, 27);
+            this.GuardarButton.TabIndex = 21;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            this.GuardarButton.Click += new System.EventHandler(this.GuardarButton_Click);
+            // 
+            // ModificarButton
+            // 
+            this.ModificarButton.Location = new System.Drawing.Point(357, 246);
+            this.ModificarButton.Name = "ModificarButton";
+            this.ModificarButton.Size = new System.Drawing.Size(75, 27);
+            this.ModificarButton.TabIndex = 20;
+            this.ModificarButton.Text = "Modificar";
+            this.ModificarButton.UseVisualStyleBackColor = true;
+            this.ModificarButton.Click += new System.EventHandler(this.ModificarButton_Click);
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Location = new System.Drawing.Point(276, 246);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(75, 27);
+            this.NuevoButton.TabIndex = 19;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            this.NuevoButton.Click += new System.EventHandler(this.NuevoButton_Click);
+            // 
+            // FechaNacimientoDateTimePicker
+            // 
+            this.FechaNacimientoDateTimePicker.Enabled = false;
+            this.FechaNacimientoDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.FechaNacimientoDateTimePicker.Location = new System.Drawing.Point(601, 27);
+            this.FechaNacimientoDateTimePicker.Name = "FechaNacimientoDateTimePicker";
+            this.FechaNacimientoDateTimePicker.Size = new System.Drawing.Size(107, 20);
+            this.FechaNacimientoDateTimePicker.TabIndex = 24;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(487, 30);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Fecha de Nacimiento:";
+            // 
+            // ClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ClientesForm";
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.FechaNacimientoDateTimePicker);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.ModificarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.EstaActivoCheckBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DireccionTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CorreoTextBox);
+            this.Controls.Add(this.TelefonoTextBox);
+            this.Controls.Add(this.IdentidadTextBox);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ClientesDataGridView);
+            this.Name = "ClientesForm";
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.ClientesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClientesDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView ClientesDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.TextBox IdentidadTextBox;
+        private System.Windows.Forms.TextBox TelefonoTextBox;
+        private System.Windows.Forms.TextBox CorreoTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DireccionTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox EstaActivoCheckBox;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button ModificarButton;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.DateTimePicker FechaNacimientoDateTimePicker;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Facturacion/Vista/ClientesForm.cs
+++ Facturacion/Vista/ClientesForm.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Datos;
+using Entidades;
+using System;
 using System.Windows.Forms;
 
 namespace Vista
@@ -15,6 +10,176 @@ namespace Vista
         public ClientesForm()
         {
             InitializeComponent();
+
+        }
+        string operacion;
+        Cliente cliente = new Cliente();
+        ClienteDB clienteDB = new ClienteDB();
+
+
+        private void HabilitarControles()
+        {
+            IdentidadTextBox.Enabled = true;
+            NombreTextBox.Enabled = true;
+            TelefonoTextBox.Enabled = true;
+            CorreoTextBox.Enabled = true;
+            DireccionTextBox.Enabled = true;
+            FechaNacimientoDateTimePicker.Enabled = true;
+            EstaActivoCheckBox.Enabled = true;
+            GuardarButton.Enabled = true;
+            CancelarButton.Enabled = true;
+            ModificarButton.Enabled = false;
+            EliminarButton.Enabled = false;
+
+
+        }
+
+        private void LimpiarControles()
+        {
+            IdentidadTextBox.Clear();
+            NombreTextBox.Clear();
+            TelefonoTextBox.Clear();
+            CorreoTextBox.Clear();
+            DireccionTextBox.Clear();
+            EstaActivoCheckBox.Checked = false;
+        }
+
+        private void DeshabilitarControles()
+        {
+            IdentidadTextBox.Enabled = false;
+            NombreTextBox.Enabled = false;
+            TelefonoTextBox.Enabled = false;
+            CorreoTextBox.Enabled = false;
+            DireccionTextBox.Enabled = false;
+            EstaActivoCheckBox.Enabled = false;
+            FechaNacimientoDateTimePicker.Enabled = false;
+            GuardarButton.Enabled = false;
+            CancelarButton.Enabled = false;
+            ModificarButton.Enabled = true;
+            EliminarButton.Enabled = true;
+        }
+
+        private void NuevoButton_Click(object sender, EventArgs e)
+        {
+            operacion = "Nuevo";
+            HabilitarControles();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            DeshabilitarControles();
+            LimpiarControles();
+        }
+
+        private void ModificarButton_Click(object sender, EventArgs e)
+        {
+            operacion = "Modificar";
+            if (ClientesDataGridView.SelectedRows.Count > 0)
+            {
+                IdentidadTextBox.Text = ClientesDataGridView.CurrentRow.Cells["Identidad"].Value.ToString();
+                NombreTextBox.Text = ClientesDataGridView.CurrentRow.Cells["Nombre"].Value.ToString();
+                TelefonoTextBox.Text = ClientesDataGridView.CurrentRow.Cells["Telefono"].Value.ToString();
+                CorreoTextBox.Text = ClientesDataGridView.CurrentRow.Cells["Correo"].Value.ToString();
+                DireccionTextBox.Text = ClientesDataGridView.CurrentRow.Cells["Direccion"].Value.ToString();
+                EstaActivoCheckBox.Checked = Convert.ToBoolean(ClientesDataGridView.CurrentRow.Cells["EstaActivo"].Value);
+                FechaNacimientoDateTimePicker.Value = Convert.ToDateTime(ClientesDataGridView.CurrentRow.Cells["FechaNacimiento"].Value);
+
+                HabilitarControles();
+            }
+        }
+
+        private void GuardarButton_Click(object sender, EventArgs e)
+        {
+            cliente.Identidad = IdentidadTextBox.Text;
+            cliente.Nombre = NombreTextBox.Text;
+            cliente.Telefono = TelefonoTextBox.Text;
+            cliente.Correo = CorreoTextBox.Text;
+            cliente.Direccion = DireccionTextBox.Text;
+            cliente.EstaActivo = EstaActivoCheckBox.Checked;
+            cliente.FechaNacimiento = FechaNacimientoDateTimePicker.Value;
+
+            if (operacion == "Nuevo")
+            {
+                if (string.IsNullOrEmpty(IdentidadTextBox.Text))
+                {
+                    errorProvider1.SetError(IdentidadTextBox, "Ingrese un código");
+                    IdentidadTextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                if (string.IsNullOrEmpty(NombreTextBox.Text))
+                {
+                    errorProvider1.SetError(NombreTextBox, "Ingrese una descripción");
+                    NombreTextBox.Focus();
+                    return;
+                }
+                errorProvider1.Clear();
+
+                bool inserto = clienteDB.Insertar(cliente);
+                if (inserto)
+                {
+                    DeshabilitarControles();
+                    LimpiarControles();
+                    TraerClientes();
+
+                    MessageBox.Show("Registro guardado con exito", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo guardar el registro", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (operacion == "Modificar")
+            {
+                bool modifico = clienteDB.Editar(cliente);
+                if (modifico)
+                {
+
+                    DeshabilitarControles();
+                    LimpiarControles();
+                    TraerClientes();
+
+                    MessageBox.Show("Registro actualizado con exito", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo actualizar el registro", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ClientesForm_Load(object sender, EventArgs e)
+        {
+            TraerClientes();
+        }
+        private void TraerClientes()
+        {
+            ClientesDataGridView.DataSource = clienteDB.DevolverClientes();
+        }
+
+        private void EliminarButton_Click(object sender, EventArgs e)
+        {
+            if (ClientesDataGridView.SelectedRows.Count > 0)
+            {
+                DialogResult resultado = MessageBox.Show("Esta seguro de eliminar el registro", "Advertencia", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    bool elimino = clienteDB.Eliminar(ClientesDataGridView.CurrentRow.Cells["Identidad"].Value.ToString());
+                    if (elimino)
+                    {
+                        LimpiarControles();
+                        DeshabilitarControles();
+                        TraerClientes();
+                        MessageBox.Show("Registro Eliminado");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar el registro");
+                    }
+                }
+            }
         }
     }
 }
+
